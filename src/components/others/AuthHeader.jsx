baf9a8b82edde92140ec005f3d9824569cc35456
kvssks <kvsskrishnasharma@gmail.com>
2026-02-09 21:45:41 +0530

    removed options from the auth header

--- src/components/others/AuthHeader.jsx
+++ src/components/others/AuthHeader.jsx
@@ -16,25 +16,6 @@ export default function AuthHeader() {
             }}
           />
         </div>
-
-        {/* Navigation Links */}
-        <nav className="auth-nav-links">
-          <Link to="/" className="auth-nav-link">
-            Home
-          </Link>
-          <a href="#features" className="auth-nav-link">
-            Features
-          </a>
-          <a href="#solutions" className="auth-nav-link">
-            Solutions
-          </a>
-          <a href="#pricing" className="auth-nav-link">
-            Pricing
-          </a>
-          <a href="mailto:support@academy51.com?subject=Contact - XVALIDATEAI" className="auth-nav-link">
-            Contact
-          </a>
-        </nav>
       </div>
     </header>
   );
