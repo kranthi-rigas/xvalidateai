ef1719907a42efe3b468792e614b4a34574ae0f6
sunilp <sunil.20453@gmail.com>
2026-02-18 16:40:29 +0530

    audit trail added

--- src/App.jsx
+++ src/App.jsx
@@ -47,8 +47,7 @@ import AILiteracyPage from "./pages/dashboard/dashboard-ailiteracy";
 import FaqsPage from "./pages/dashboard/dashboard-faqs";
 import SubscriptionSuccess from "./components/dashboard/Billing/SubscriptionSuccess";
 import SubscriptionCancel from "./components/dashboard/Billing/SubscriptionCancel";
-import PrivacyPolicyPage from "./pages/others/privacy-policy";
-import TermsPage from "./pages/others/terms";
+import AuditLogsPage from "./pages/dashboard/dashboard-audittrail";
 
 function App() {
   useEffect(() => {
@@ -104,7 +103,9 @@ function App() {
                 <Route path="dashboard" element={<DshbDashboardPage />} />
                 <Route index element={<AIDashboardPage />} />
                 <Route path="ailiteracy" element={<AILiteracyPage />} />
+
                 <Route path="faqs" element={<FaqsPage />} />
+                <Route path="audittrail" element={<AuditLogsPage />} />
                 <Route path="forums" element={<DshbForumsPage />} />
                 <Route path="grades" element={<DshbGradesPage />} />
                 <Route path="messages" element={<DshbMessagesPage />} />
